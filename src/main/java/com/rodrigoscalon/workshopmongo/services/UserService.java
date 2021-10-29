ba33dffb82f738ec9e8d7af28244536255b708cf
Rodrigo Scalon <rodrigoscalon@gmail.com>
2021-10-29 13:41:25 -0300

    Add userId

--- src/main/java/com/rodrigoscalon/workshopmongo/services/UserService.java
+++ src/main/java/com/rodrigoscalon/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rodrigoscalon.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrigoscalon.workshopmongo.domain.User;
 import com.rodrigoscalon.workshopmongo.repository.UserRepository;
+import com.rodrigoscalon.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,4 +21,10 @@ public class UserService {
 	return repo.findAll();	
 		
 	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+
 }
