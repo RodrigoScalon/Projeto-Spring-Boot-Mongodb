76d7a044836f2bbb1edcf0761f2ba566cb385b82
Rodrigo Scalon <rodrigoscalon@gmail.com>
2021-10-29 12:17:14 -0300

    Repository and Services - MONGODB

--- src/main/java/com/rodrigoscalon/workshopmongo/resources/UserResource.java
+++ src/main/java/com/rodrigoscalon/workshopmongo/resources/UserResource.java
@@ -1,28 +1,26 @@
 package com.rodrigoscalon.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodrigoscalon.workshopmongo.domain.User;
+import com.rodrigoscalon.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping (value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity <List<User>> findAll () {
-		
-		User batman = new User("1", "Bruce Wayne", "bruce@gmail.com");
-		User wolverine = new User("2", "Logan", "logan@gmail.com");
-		List <User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(batman, wolverine));
-		
+		List <User> list = service.findAll();
 		return ResponseEntity.ok().body(list);		
 		
 	}
---
+++ src/main/java/com/rodrigoscalon/workshopmongo/services/UserService.java
@@ -0,0 +1,22 @@
+package com.rodrigoscalon.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigoscalon.workshopmongo.domain.User;
+import com.rodrigoscalon.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo; 
+	
+	
+	public List<User> findAll(){
+	return repo.findAll();	
+		
+	}
+}
